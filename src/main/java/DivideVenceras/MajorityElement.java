fd677433fb4d4ceaeabf1feec3e5c2713fad2db1
Lamsu1505 <131729688+Lamsu1505@users.noreply.github.com>
2025-10-30 20:49:22 -0500

    Punto 1 implementado

---
+++ src/main/java/DivideVenceras/MajorityElement.java
@@ -0,0 +1,40 @@
+package DivideVenceras;
+
+public class MajorityElement{
+
+    public static int findMajorityElement(int[] nums) {
+        return majorityElementRec(nums, 0, nums.length - 1);
+    }
+
+    private static int majorityElementRec(int[] nums, int left, int right) {
+        // Caso base: solo un elemento
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = (left + right) / 2;
+
+        // Divide
+        int leftMajor = majorityElementRec(nums, left, mid);
+        int rightMajor = majorityElementRec(nums, mid + 1, right);
+
+        // Conquista
+        if (leftMajor == rightMajor) {
+            return leftMajor;
+        }
+
+        // Cuenta cuántas veces aparece cada uno
+        int leftCount = countInRange(nums, leftMajor, left, right);
+        int rightCount = countInRange(nums, rightMajor, left, right);
+
+        return leftCount > rightCount ? leftMajor : rightMajor;
+    }
+
+    private static int countInRange(int[] nums, int num, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == num) count++;
+        }
+        return count;
+    }
+}
--- src/main/java/Punto1/Punto1.java
+++
@@ -1,7 +0,0 @@
-package Punto1;
-
-public class Punto1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
